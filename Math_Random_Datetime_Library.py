f07d318ef515e8f573c1d139c87c201e75e60683
Akash Raut <136334052+Akash-Raut2000@users.noreply.github.com>
2024-12-18 22:55:13 +0530

    Add files via upload

---
+++ Math_Random_Datetime_Library.py
@@ -0,0 +1,15 @@
+import math  # For mathematical functions
+import random  # For generating random numbers
+import datetime  # For working with dates and times
+
+# Using the math module
+square_root = math.sqrt(16)  # Calculates the square root of 16
+print("Square root of 16:", square_root)
+
+# Using the random module
+random_number = random.randint(1, 10)  # Generates a random number between 1 and 10
+print("Random number:", random_number)
+
+# Using the datetime module
+current_time = datetime.datetime.now()  # Gets the current date and time
+print("Current date and time:", current_time)
